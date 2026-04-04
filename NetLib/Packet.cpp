ee8a52355294dd8052d07d9628c42f73f2fac34a
PC <aa01053547426@gmail.com>
2026-04-04 18:28:22 +0900

    example

---
+++ NetLib/Packet.cpp
@@ -0,0 +1,488 @@
+#include "Packet.h"
+
+SerializeBuffer::SerializeBuffer()
+{
+	int bufferSize = PAYLOAD_LEN_DEFAULT;
+	mBuffer = new char[bufferSize + 1];
+	mBufferSize = bufferSize;
+	mDataSize = 0;
+	mBufferEnd = mBuffer + mBufferSize;
+	mFront = mBuffer;
+	mRear = mBuffer;
+	//lastError = eErrorCode::NONE;
+}
+SerializeBuffer::SerializeBuffer(int bufferSize)
+{
+	mBuffer = new char[bufferSize + 1];
+	mBufferSize = bufferSize;
+	mDataSize = 0;
+	mBufferEnd = mBuffer + mBufferSize;
+
+	mFront = mBuffer;
+	mRear = mBuffer;
+	//lastError = eErrorCode::NONE;
+}
+
+SerializeBuffer::~SerializeBuffer()
+{
+	delete[] mBuffer;
+}
+
+/// <summary>
+/// 대입
+/// </summary>
+/// <param name="clSrcPacket"></param>
+/// <returns></returns>
+SerializeBuffer& SerializeBuffer::operator = (SerializeBuffer& srcPacket)
+{
+	// TODO 미정.
+	// 주체의 크기가 더 작은 경우 손실시킬지 아예 하지 않을지.
+	return *this;
+}
+
+/// <summary>
+/// uchar 삽입
+/// </summary>
+/// <param name="value"></param>
+/// <returns></returns>
+SerializeBuffer& SerializeBuffer::operator << (unsigned char value)
+{
+	if (mBufferEnd < mRear + sizeof(unsigned char))
+	{
+		//lastError = eErrorCode::FAILED_WRITE;
+	}
+	else
+	{
+		*(unsigned char*)mRear = value;
+		mRear += sizeof(unsigned char);
+		mDataSize += sizeof(unsigned char);
+	}
+
+	return *this;
+}
+
+/// <summary>
+/// char 삽입
+/// </summary>
+/// <param name="value"></param>
+/// <returns></returns>
+SerializeBuffer& SerializeBuffer::operator << (char value)
+{
+	if (mBufferEnd < mRear + sizeof(char))
+	{
+		//lastError = eErrorCode::FAILED_WRITE;
+	}
+	else
+	{
+		*(char*)mRear = value;
+		mRear += sizeof(char);
+		mDataSize += sizeof(char);
+	}
+
+	return *this;
+}
+
+/// <summary>
+/// short 삽입
+/// </summary>
+/// <param name="value"></param>
+/// <returns></returns>
+SerializeBuffer& SerializeBuffer::operator << (short value)
+{
+	if (mBufferEnd < mRear + sizeof(short))
+	{
+		//lastError = eErrorCode::FAILED_WRITE;
+	}
+	else
+	{
+		*(short*)mRear = value;
+		mRear += sizeof(short);
+		mDataSize += sizeof(short);
+	}
+
+	return *this;
+}
+
+
+
+/// <summary>
+/// int 삽입
+/// </summary>
+/// <param name="value"></param>
+/// <returns></returns>
+SerializeBuffer& SerializeBuffer::operator << (int value)
+{
+	if (mBufferEnd < mRear + sizeof(int))
+	{
+		//lastError = eErrorCode::FAILED_WRITE;
+	}
+	else
+	{
+		*(int*)mRear = value;
+		mRear += sizeof(int);
+		mDataSize += sizeof(int);
+	}
+
+	return *this;
+}
+
+/// <summary>
+/// long 삽입
+/// </summary>
+/// <param name="value"></param>
+/// <returns></returns>
+SerializeBuffer& SerializeBuffer::operator << (long value)
+{
+	if (mBufferEnd < mRear + sizeof(long))
+	{
+		//lastError = eErrorCode::FAILED_WRITE;
+	}
+	else
+	{
+		*(long*)mRear = value;
+		mRear += sizeof(long);
+		mDataSize += sizeof(long);
+	}
+
+	return *this;
+}
+
+/// <summary>
+/// float 삽입
+/// </summary>
+/// <param name="value"></param>
+/// <returns></returns>
+SerializeBuffer& SerializeBuffer::operator << (float value)
+{
+	if (mBufferEnd < mRear + sizeof(float))
+	{
+		//lastError = eErrorCode::FAILED_WRITE;
+	}
+	else
+	{
+		*(float*)mRear = value;
+		mRear += sizeof(float);
+		mDataSize += sizeof(float);
+	}
+
+	return *this;
+}
+
+
+
+/// <summary>
+/// double 삽입
+/// </summary>
+/// <param name="value"></param>
+/// <returns></returns>
+SerializeBuffer& SerializeBuffer::operator << (double value)
+{
+	if (mBufferEnd < mRear + sizeof(double))
+	{
+		//lastError = eErrorCode::FAILED_WRITE;
+	}
+	else
+	{
+		*(double*)mRear = value;
+		mRear += sizeof(double);
+		mDataSize += sizeof(double);
+	}
+
+	return *this;
+}
+SerializeBuffer& SerializeBuffer::operator << (unsigned long long value)
+{
+	if (mBufferEnd < mRear + sizeof(unsigned long long))
+	{
+		//lastError = eErrorCode::FAILED_WRITE;
+	}
+	else
+	{
+		*(unsigned long long*)mRear = value;
+		mRear += sizeof(unsigned long long);
+		mDataSize += sizeof(unsigned long long);
+	}
+
+	return *this;
+}
+SerializeBuffer& SerializeBuffer::operator << (DWORD value)
+{
+	if (mBufferEnd < mRear + sizeof(DWORD))
+	{
+		//lastError = eErrorCode::FAILED_WRITE;
+	}
+	else
+	{
+		*(DWORD*)mRear = value;
+		mRear += sizeof(DWORD);
+		mDataSize += sizeof(DWORD);
+	}
+
+	return *this;
+}
+/// <summary>
+/// BYTE(uchar) 추출
+/// </summary>
+/// <param name="value"></param>
+/// <returns></returns>
+SerializeBuffer& SerializeBuffer::operator >> (BYTE& value)
+{
+	if (mDataSize < sizeof(BYTE))
+	{
+		//lastError = eErrorCode::FAILED_READ;
+	}
+	else
+	{
+		value = *(BYTE*)mFront;
+		mFront += sizeof(BYTE);
+		mDataSize -= sizeof(BYTE);
+	}
+
+	return *this;
+}
+
+/// <summary>
+/// char 추출
+/// </summary>
+/// <param name="value"></param>
+/// <returns></returns>
+SerializeBuffer& SerializeBuffer::operator >> (char& value)
+{
+	if (mDataSize < sizeof(char))
+	{
+		//lastError = eErrorCode::FAILED_READ;
+	}
+	else
+	{
+		value = *(char*)mFront;
+		mFront += sizeof(char);
+		mDataSize -= sizeof(char);
+	}
+
+	return *this;
+}
+
+/// <summary>
+/// short 추출
+/// </summary>
+/// <param name="value"></param>
+/// <returns></returns>
+SerializeBuffer& SerializeBuffer::operator >> (short& value)
+{
+	if (mDataSize < sizeof(short))
+	{
+		//lastError = eErrorCode::FAILED_READ;
+	}
+	else
+	{
+		value = *(short*)mFront;
+		mFront += sizeof(short);
+		mDataSize -= sizeof(short);
+	}
+
+	return *this;
+}
+
+/// <summary>
+/// WORD(ushort) 추출
+/// </summary>
+/// <param name="value"></param>
+/// <returns></returns>
+SerializeBuffer& SerializeBuffer::operator >> (WORD& value)
+{
+	if (mDataSize < sizeof(WORD))
+	{
+		//lastError = eErrorCode::FAILED_READ;
+	}
+	else
+	{
+		value = *(WORD*)mFront;
+		mFront += sizeof(WORD);
+		mDataSize -= sizeof(WORD);
+	}
+
+	return *this;
+}
+
+/// <summary>
+/// int 추출
+/// </summary>
+/// <param name="value"></param>
+/// <returns></returns>
+SerializeBuffer& SerializeBuffer::operator >> (int& value)
+{
+	if (mDataSize < sizeof(int))
+	{
+		//lastError = eErrorCode::FAILED_READ;
+	}
+	else
+	{
+		value = *(int*)mFront;
+		mFront += sizeof(int);
+		mDataSize -= sizeof(int);
+	}
+
+	return *this;
+}
+
+/// <summary>
+/// DWORD(ulong)추출
+/// </summary>
+/// <param name="value"></param>
+/// <returns></returns>
+SerializeBuffer& SerializeBuffer::operator >> (DWORD& value)
+{
+	if (mDataSize < sizeof(DWORD))
+	{
+		//lastError = eErrorCode::FAILED_READ;
+	}
+	else
+	{
+		value = *(DWORD*)mFront;
+		mFront += sizeof(DWORD);
+		mDataSize -= sizeof(DWORD);
+	}
+
+	return *this;
+}
+
+/// <summary>
+/// float 추출
+/// </summary>
+/// <param name="value"></param>
+/// <returns></returns>
+SerializeBuffer& SerializeBuffer::operator >> (float& value)
+{
+	if (mDataSize < sizeof(float))
+	{
+		//lastError = eErrorCode::FAILED_READ;
+	}
+	else
+	{
+		value = *(float*)mFront;
+		mFront += sizeof(float);
+		mDataSize -= sizeof(float);
+	}
+
+	return *this;
+}
+
+/// <summary>
+/// __int64 추출
+/// </summary>
+/// <param name="value"></param>
+/// <returns></returns>
+SerializeBuffer& SerializeBuffer::operator >> (__int64& value)
+{
+	if (mDataSize < sizeof(__int64))
+	{
+		//lastError = eErrorCode::FAILED_READ;
+	}
+	else
+	{
+		value = *(__int64*)mFront;
+		mFront += sizeof(__int64);
+		mDataSize -= sizeof(__int64);
+	}
+
+	return *this;
+}
+
+SerializeBuffer& SerializeBuffer::operator >> (unsigned long long& value)
+{
+	if (mDataSize < sizeof(unsigned long long))
+	{
+		//lastError = eErrorCode::FAILED_READ;
+	}
+	else
+	{
+		value = *(unsigned long long*)mFront;
+		mFront += sizeof(unsigned long long);
+		mDataSize -= sizeof(unsigned long long);
+	}
+
+	return *this;
+}
+
+/// <summary>
+/// double 추출
+/// </summary>
+/// <param name="value"></param>
+/// <returns></returns>
+SerializeBuffer& SerializeBuffer::operator >> (double& value)
+{
+	if (mDataSize < sizeof(double))
+	{
+		//lastError = eErrorCode::FAILED_READ;
+	}
+	else
+	{
+		value = *(double*)mFront;
+		mFront += sizeof(double);
+		mDataSize -= sizeof(double);
+	}
+
+	return *this;
+}
+
+/// <summary>
+/// 데이터를 얻는다.
+/// </summary>
+/// <param name="dest">데이터를 복사할 포인터</param>
+/// <param name="size">크기</param>
+/// <returns>얻는데 성공한 크기</returns>
+int	SerializeBuffer::GetData(char* dest, int size)
+{
+	if (size > mDataSize)
+		size = mDataSize;
+
+	memcpy(dest, mFront, size);
+	mFront += size;
+	mDataSize -= size;
+
+	return size;
+}
+
+
+
+
+
+int SerializeBuffer::MoveFront(int size)
+{
+	if (size > mDataSize)
+		size = mDataSize;
+	mFront += size;
+	mDataSize -= size;
+
+	return size;
+}
+
+int SerializeBuffer::MoveRear(int size)
+{
+	if (mBufferEnd < mRear + size)
+	{
+		//lastError = eErrorCode::FAILED_WRITE;
+		size = 0;
+	}
+	else
+	{
+		mRear += size;
+		mDataSize += size;
+	}
+
+	return size;
+}
+
+int SerializeBuffer::Expand()
+{
+	int expandSize = mBufferSize * 2;
+	char* temp = new char[expandSize + 1];
+	memcpy(temp, mBuffer, mBufferSize);
+	mFront = temp + (mFront - mBuffer);
+	mRear = temp + (mRear - mBuffer);
+	delete[] mBuffer;
+	mBuffer = temp;
+	mBufferSize = expandSize;
+	mBufferEnd = mBuffer + expandSize;
+	return expandSize;
+}
---
+++ main.cpp
@@ -0,0 +1,147 @@
+﻿#include <iostream>
+#include "NetLib/NetLib_Server.h"
+#include "Utils/Parser.h"
+#include "conio.h"
+#include "Utils/SystemMonitor.h"
+
+class ExampleServer :public NetLib_Server
+{
+public:
+	ExampleServer(const WCHAR* openIP,
+		unsigned short openPort,
+		int opt_workerTH_Pool_size,
+		int opt_concurrentTH_size,
+		unsigned short opt_maxOfSession,
+		bool opt_zerocpy,
+		Opt_Encryption* opt_encryption,
+		int opt_maxOfSendPackets)
+		:NetLib_Server(openIP,
+			openPort,
+			opt_workerTH_Pool_size,
+			opt_concurrentTH_size,
+			opt_maxOfSession,
+			opt_zerocpy,
+			opt_encryption,
+			opt_maxOfSendPackets)
+	{
+
+	}
+
+	virtual void OnClientJoin(unsigned long long sessionHandle)
+	{
+
+		return;
+	}
+
+	virtual void OnClientLeave(unsigned long long sessionHandle)
+	{
+		return;
+	}
+
+	virtual bool OnConnectionRequest(unsigned long IP, unsigned short port)
+	{
+		// 다 받음
+		return true;
+	}
+
+	virtual void OnRecv(unsigned long long sessionHandle, Packet* packet)
+	{
+		PROFILING("CONTENT");
+
+		WORD contentType;
+		*packet >> contentType;
+		Content_Proc(sessionHandle, contentType, packet);
+	}
+
+	void Content_Proc(unsigned long long sessionHandle, WORD contentType, Packet* packet)
+	{
+		switch (contentType)
+		{
+		case 0:
+		{
+			long long echoData;
+			*packet >> echoData;
+			Packet* echoPacket = Packet::Alloc();
+			*echoPacket << (WORD)contentType;
+			*echoPacket << echoData;
+			SendPacket(sessionHandle, packet);
+			Packet::Free(echoPacket);
+			break;
+		}
+		default:
+		{
+			break;
+		}
+		}
+	}
+
+	SystemMonitor system_monitor;
+};
+
+ExampleServer* exampleserver;
+
+int main()
+{
+	int opt_serverport;
+	int opt_poolsize;
+	int opt_concurrentsize;
+	int opt_maxofsession;
+	bool opt_zerocpy;
+	char opt_encryption_header_code;
+	char opt_encryption_fixed_key;
+	int opt_sendbuf;
+	Parser parser;
+	if (parser.loadFromFile("server_config.cfg"))
+	{
+		opt_serverport = parser.GetInt("port");
+		opt_poolsize = parser.GetInt("workerTH_Pool_size");
+		opt_concurrentsize = parser.GetInt("concurrentTH_size");
+		opt_maxofsession = parser.GetInt("maxofsession");
+
+		opt_encryption_header_code = (char)parser.GetInt("encryption_header_code");
+		opt_encryption_fixed_key = (char)parser.GetInt("encryption_fixed_key");
+
+		opt_zerocpy = parser.GetBool("zerocopy");
+		opt_sendbuf = parser.GetInt("sendbuf");
+	}
+	else
+	{
+		printf("config파일이 없습니다!!!");
+		wint_t key = _getwch();
+		return -1;
+	}
+
+	Opt_Encryption opt_encryption = { opt_encryption_header_code, opt_encryption_fixed_key };
+	exampleserver = new ExampleServer(L"0.0.0.0", opt_serverport, opt_poolsize, opt_concurrentsize,
+		opt_maxofsession, opt_zerocpy, &opt_encryption, opt_sendbuf);
+
+	bool isRunning = true;
+
+	exampleserver->Start();
+	printf("Server Start\n");
+
+	while (isRunning)
+	{
+		printf("Q누르면 종료\n");
+		printf("\n\nSession Count:              %7d", exampleserver->GetSessionCount());
+		printf("\nPacket Pool Use:            %7d", exampleserver->GetPacketUseSize());
+		printf("\nTotal_Accept:               %7d", exampleserver->GetTotal_Accept());
+		printf("\nTPS_Accept:                 %7d", exampleserver->GetTPS_Accept());
+		printf("\nTPS_Send:                   %7d", exampleserver->GetTPS_Send());
+		printf("\nTPS_Recv:                   %7d", exampleserver->GetTPS_Recv());
+		printf("\n-------------------------------------------------------------------------\n");
+
+		if (_kbhit())
+		{
+			wint_t key = _getwch();
+
+			if (key == 'q' || key == 'Q')
+			{
+				isRunning = false;
+			}
+
+
+		}
+		Sleep(1000);
+	}
+}
